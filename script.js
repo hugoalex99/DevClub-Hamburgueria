b31d2c08d93e82c99ed168a404c1c827751ae5d7
Hugo Alexandre <102272134+hugoalex99@users.noreply.github.com>
2025-05-19 22:45:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
+const sumAll = document.querySelector('.sum-all')
+const filterAll = document.querySelector('.filter-all')
 
 
-function showAll(productsArray){
+function formatCurrency(value) {
+    const newValue = value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+
+    return newValue
+}
+
+function showAll(productsArray) {
 
     myLi = ''
     productsArray.forEach(product => {
-         myLi += `
+        myLi += `
     
-        <li>
-             <img src=${product.src}>
-            <p>${product.name}</p>
-            <p class="item-price">R$ ${product.price}</p>
-         </li>
+            <li>
+                <img src=${product.src}>
+                <p>${product.name}</p>
+                <p class="item-price">${formatCurrency(product.price)}</p>
+            </li>
     
     `
     })
@@ -21,15 +32,32 @@ function showAll(productsArray){
     list.innerHTML = myLi
 }
 
-function mapAllItems(){
+function mapAllItems() {
     const newPrices = menuOptions.map((product) => ({
         ...product, //Spread Operator
         price: product.price * 0.9, //10% de desconto
     }))
 
     showAll(newPrices)
+
+}
+function sumAllItems() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+   list.innerHTML = `
+        <li>
+            <p>O valor total dos itens é R$ ${formatCurrency(totalValue)}</p>
+        </li>
     
+    `
+}
+function filterAllItems() {
+    const filterJustVegan = menuOptions.filter(product => product.vegan)
+    showAll(filterJustVegan)
 }
 
+
 buttonShowAll.addEventListener('click', () => showAll(menuOptions))
-buttonMapAll.addEventListener('click',  mapAllItems)
+buttonMapAll.addEventListener('click', mapAllItems)
+sumAll.addEventListener('click', sumAllItems)
+filterAll.addEventListener('click', filterAllItems)
